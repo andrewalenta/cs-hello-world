5cf10be10ee85c03d8918a2a06930d22dcdc5234
andrewalenta <info@datandre.de>
2024-03-05 22:58:44 +0100

    initial

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MeinCSharpProjekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var meineKlasse = new MeineKlasse();
+            meineKlasse.MeineMethode();
+        }
+    }
+}
---
+++ myClassTest.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MeinCSharpProjekt
+{
+    class MeineKlasse
+    {
+        public void MeineMethode()
+        {
+            Console.WriteLine("Hallo Welt!");
+        }
+    }
+}
